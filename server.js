24dff534ffa6dcdc9643df5ed6f49e56bcd5ac13
ynfleozin <leonardoalvarengam@gmail.com>
2024-12-08 12:45:21 -0300

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.static("uploads"))
+
+app.use(cors());
+
+app.use(express.static("uploads"));
 routes(app);
 
 app.listen(3000, () => {
